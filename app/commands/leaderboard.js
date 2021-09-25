6748f9c3d1d356974827108089f80231cda1e78a
Tim <timbretz94@gmail.com>
2021-09-25 16:02:42 -0500

    refactor leaderboard to use promise chain

--- app/commands/leaderboard.js
+++ app/commands/leaderboard.js
@@ -6,13 +6,8 @@ module.exports = {
         .setName('leaderboard')
         .setDescription('Shows the top members'),
     async execute(interaction) {
-        let members = await interaction.guild.members.fetch();
-        let users = await User.findAll({ where: { guild: interaction.guild.id } });
-
-        users = users.filter(u => u.guild == interaction.guild.id);
-        users.sort((u1, u2) => u2.balance - u1.balance);
-        users = users.slice(0,3);
-
-        await interaction.reply(`${users.map(u => `${members.find(m => m.user.tag == u.username)} \t ${u.balance}`).join('\n')}`);
+        User.findAll({ where: { guild: interaction.guild.id } })
+            .then(users => interaction.guild.members.fetch()
+                .then(members => interaction.reply(`${users.map(u => `${members.find(m => m.user.tag == u.username)} \t ${u.balance}`).join('\n')}`)));
     }
 };
