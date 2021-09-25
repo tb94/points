80bfafb792b4115a22f4dfd2443a339d7e86bc5a
Tim <timbretz94@gmail.com>
2021-09-25 16:05:48 -0500

    order and limit leaderboard to top 3

--- app/commands/leaderboard.js
+++ app/commands/leaderboard.js
@@ -6,8 +6,11 @@ module.exports = {
         .setName('leaderboard')
         .setDescription('Shows the top members'),
     async execute(interaction) {
-        User.findAll({ where: { guild: interaction.guild.id } })
-            .then(users => interaction.guild.members.fetch()
-                .then(members => interaction.reply(`${users.map(u => `${members.find(m => m.user.tag == u.username)} \t ${u.balance}`).join('\n')}`)));
+        User.findAll({
+            where: { guild: interaction.guild.id },
+            order: [['balance', 'DESC']],
+            limit: 3
+        }).then(users => interaction.guild.members.fetch()
+            .then(members => interaction.reply(`${users.map(u => `${members.find(m => m.user.tag == u.username)} \t ${u.balance}`).join('\n')}`)));
     }
 };
