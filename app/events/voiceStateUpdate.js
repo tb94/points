b2a14dd72a802fc65e2dac536b5ce10330852db1
Tim <timbretz94@gmail.com>
2021-09-25 21:29:33 -0500

    voiceStateUpdate refactor
    state needs to be addressed
    anything that happens to voice state while the bot is
    offline/busy/etc is not reassessed and could cause inconsistencies

--- app/events/voiceStateUpdate.js
+++ app/events/voiceStateUpdate.js
@@ -3,24 +3,14 @@ const { User, VoiceState } = require("../db/models");
 module.exports = {
     name: 'voiceStateUpdate',
     async execute(oldState, newState) {
-        console.log(`${oldState.member.user.username} going from ${oldState.channelId} to ${newState.channelId} as ${newState.member.user.username}`);
-
-        // switching guilds (create new voice state and destry old)
-        if (oldState.guild.id !== newState.guild.id) {
-            leaveChat(oldState).then(() => joinChat(newState));
-            return;
-        }
-
-        // joining a voice chat
-        if (oldState.channelId == null) {
-            joinChat(newState);
-            return;
-        }
-
-        // leaving a voice chat
-        if (newState.channelId == null || newState.channelId == newState.guild.afkChannelId) {
-            leaveChat(oldState);
-            return;
+        if ((oldState.channel.id == null || oldState.channel.id == oldState.guild.afkChannel.id)
+            && (newState.channel.id != null || newState.channel.id == newState.guild.afkChannel.id)) {
+            await joinChat(newState);
+        } else if ((newState.channel.id == null || newState.channel.id == newState.guild.afkChannel.id)
+        && (oldState.channel.id != null || oldState.channel.id == oldState.guild.afkChannel.id)) {
+            await leaveChat
+        } else if (oldState.guild.id != newState.guild.id) {
+            await leaveChat(oldState).then(() => joinChat(newState));
         }
     }
 }
@@ -36,7 +26,7 @@ function joinChat(newState) {
 function leaveChat(oldState) {
     const t = Date.now();
 
-    return User.findCreateFind({ where: { username: oldState.member.user.tag, guild: oldState.guild.id }, include: VoiceState })
+    return User.findOne({ where: { username: oldState.member.user.tag, guild: oldState.guild.id }, include: VoiceState })
         .then(([user]) => user.getVoiceState()
             .then(voiceState => user.increment('balance', { by: Math.floor((t - (voiceState?.timestamp ?? t)) / 60000) })
                 .then(() => voiceState?.destroy())))
