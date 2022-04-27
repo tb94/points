dfd7fc292fc4aa9f40dfc7c2cffb83a365ce2f54
Tim <timbretz94@gmail.com>
2022-04-26 20:06:28 -0500

    ad hoc bot user cleanup

--- app/events/guildMemberAdd.js
+++ app/events/guildMemberAdd.js
@@ -3,6 +3,7 @@ const { User } = require("../db/models");
 module.exports = {
     name: 'guildMemberAdd',
     async execute(guildMember) {
+        if (!guildMember.user.bot)
         await User.findCreateFind({
             where: { username: guildMember.user.tag, guild: guildMember.guild.id }
         });
