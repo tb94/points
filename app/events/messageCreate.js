f000998054fc67143359e388376e8428a273e46b
Tim <timbretz94@gmail.com>
2022-04-26 20:35:27 -0500

    this is getting squashed later

--- app/events/messageCreate.js
+++ app/events/messageCreate.js
@@ -3,7 +3,7 @@ const { User } = require("../db/models");
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        if (!guildMember.user.bot)
+        if (!message.author.bot)
         User.findCreateFind({ where: { username: message.author.tag, guild: message.guild.id } })
             .then(([user, isNew]) => user.increment('balance'));
     }
