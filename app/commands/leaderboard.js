4737518f010e1c5a8d9f789718c83447d49b38ab
Tim <timbretz94@gmail.com>
2022-04-27 14:59:07 -0500

    inline

--- app/commands/leaderboard.js
+++ app/commands/leaderboard.js
@@ -23,12 +23,11 @@ module.exports = {
                 var place = 0;
                 var embed = new MessageEmbed()
                 .setTitle("Leaderboard")
-                .setDescription("\u200B");
 
                 for (var user of users) {
                     member = members.find(m => m.user.tag == user.username);
                     embed.addFields(
-                        { name: "\u200b", value: "\u200b"},
+                        { name: "\u200b", value: "\u200b", inline: true},
                         { name: `${medals[place]}`, value: `${member.user} \t ${user.balance} 💰`});
                     place++;
                 }
