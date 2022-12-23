4b213d09f07bf3945f850b8eb3871f5a8d689a5d
Tim <timbretz94@gmail.com>
2022-12-23 15:56:32 -0600

    voice state refactor

--- app/events/voiceStateUpdate.js
+++ app/events/voiceStateUpdate.js
@@ -4,39 +4,50 @@ const { User } = require("../db/models");
 module.exports = {
     name: Events.VoiceStateUpdate,
     async execute(oldState, newState) {
-        if ((oldState.channel?.id == null || oldState.channel?.id == oldState.guild?.afkChannel.id)
-            && (newState.channel?.id != null || newState.channel?.id == newState.guild?.afkChannel.id)) {
-            await joinChat(newState);
-        } else if ((newState.channel?.id == null || newState.channel?.id == newState.guild?.afkChannel.id)
-            && (oldState.channel?.id != null || oldState.channel?.id == oldState.guild?.afkChannel.id)) {
+        let oldChannelId = oldState.channelId ?? oldState.channel?.id;
+        let newChannelId = newState.channelId ?? newState.channel?.id;
+
+        if (oldChannelId == newChannelId) {
+            // no change
+        } else if (newChannelId == null) {
+            // disconnect
             await leaveChat(oldState);
-        } else if (oldState.guild?.id != newState.guild?.id) {
-            await leaveChat(oldState).then(() => joinChat(newState));
+        } else if (newChannelId == newState.guild.afkChannelId ?? oldState.guild.afkChannel?.id) {
+            // moved to afk
+            await leaveChat(oldState);
+        } else if (oldChannelId == null) {
+            // fresh join
+            await joinChat(newState);
+        } else if (oldChannelId == oldState.guild.afkChannelId ?? oldState.guild.afkChannel?.id) {
+            // join from afk
+            await joinChat(newState);
+        } else if (oldState.guild.id == newState.guild.id) {
+            // join from another channel in the same guild 
         } else {
-            console.error("something weird happened with voiceStateUpdate");
+            console.error("something weird happened with voice state")
+            console.error("oldState: %s", JSON.stringify(oldState, undefined, 2));
+            console.error("newState: %s", JSON.stringify(newState, undefined, 2));
         }
     }
 }
 
-function joinChat(state) {
+function joinChat(voiceState) {
     const t = Date.now();
-    if (!state.member.user.bot)
+    if (!voiceState.member.user.bot)
         return User.findCreateFind({
-            where: { snowflake: state.member.user.id, guild: state.guild.id },
-            defaults: { username: state.member.user.tag }
+            where: { snowflake: voiceState.member.user.id, guild: voiceState.guild.id },
+            defaults: { username: voiceState.member.user.tag }
         })
-            .then(([u]) => u.set('voiceActivity', t))
-            .then(u => u.save())
+            .then(([u]) => u.update({ voiceActivity: t }))
             .catch(err => console.error(err));
 }
 
-function leaveChat(state) {
-    if (!state.member.user.bot)
+function leaveChat(voiceState) {
+    if (!voiceState.member.user.bot)
         return User.findCreateFind({
-            where: { snowflake: state.member.user.id, guild: state.guild.id },
-            defaults: { username: state.member.user.tag }
+            where: { snowflake: voiceState.member.user.id, guild: voiceState.guild.id },
+            defaults: { username: voiceState.member.user.tag }
         })
-            .then(([u]) => u.set('voiceActivity', null))
-            .then(u => u.save())
+            .then(([u]) => u.update({ voiceActivity: null }))
             .catch(err => console.error(err));
 }
